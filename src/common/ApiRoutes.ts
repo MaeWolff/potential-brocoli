2fdd375e8445eda5635a99e5b5eb50c82c803508
MaevaWolff <maeva.wolff@hetic.net>
2021-04-05 17:00:42 +0200

    feat: preparation of fetch/hook files

---
+++ src/common/ApiRoutes.ts
@@ -0,0 +1,10 @@
+export const apiHost = "http://localhost:3001";
+
+export enum ApiRoutes {}
+
+/* ---- OTHER WAY TO WRITE IT -----
+   export const ApiRoutes = {
+    USER_REGISTER: "/register",
+  } as const;  */
+
+// DOCU : const as const (https://blog.logrocket.com/const-assertions-are-the-killer-new-typescript-feature-b73451f35802/)
