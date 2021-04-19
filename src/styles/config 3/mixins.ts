d3d65b44ddf36cff471af1a2bc6cf78be193687b
MaevaWolff <maeva.wolff@hetic.net>
2021-04-19 23:43:45 +0200

    remove useless folder

--- src/styles/config 3/mixins.ts
+++
@@ -1,7 +0,0 @@
-import { css } from "styled-components";
-
-export const BackgroundPrimaryText = css`
-  background-color: ${({ theme }) => theme.colors.primary100};
-  padding: 0em 0.25em;
-  border-radius: 8px;
-`;
