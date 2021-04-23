cad2a65398dc030c0fb3d724d0ef6a5a6a1979cd
bryanIroko <bryan@iroko.eu>
2021-04-23 13:28:56 +0200

    mono

--- src/components/forms/formValidationConstants.ts
+++
@@ -1,7 +0,0 @@
-export const REQUIRED_MSG = "Ce champ est requis";
-export const TOO_SHORT_MSG = "Ce champ est trop court";
-
-export const EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
-export const INVALID_EMAIL_MSG = "Merci de renseigner une adresse email valide";
-
-export const MUST_MATCH_PASSWORD_MSG = "Le mot de passe ne correspond pas";
